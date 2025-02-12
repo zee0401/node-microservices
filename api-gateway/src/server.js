98807abfcd94634276edc96aa34082f3a64254e6
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-12 11:53:30 +0530

    eslint

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -1,11 +1,12 @@
-import express from 'express'
+import express from "express";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+app.use(cors());
+
+app.listen(3000);
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            "no-unused-vars": "warn",
+        },
+    },
 ];
