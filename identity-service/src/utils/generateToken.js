be683b03fbc8dcbd88dfcb9098464e47e4c20d86
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-12 19:40:14 +0530

    generateToken

---
+++ identity-service/src/utils/generateToken.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import RefreshToken from "../models/refreshTokenModel";
+
+const generateToken = async (user) => {
+    const accessToken = jwt.sign(
+        { user: user._id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: "60m" }
+    );
+
+    const refreshToken = crypto.randomBytes(40).toString("hex");
+
+    const expiresAt = new Date();
+    expiresAt.setDate(expiresAt.getDate() + 7);
+    await RefreshToken.create({
+        token: refreshToken,
+        user: user._id,
+        expiresAt,
+    });
+
+    return { accessToken, refreshToken };
+};
+
+export default generateToken;
