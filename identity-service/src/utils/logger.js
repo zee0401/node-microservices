6937802dd06d3b6fa88944eee20fa8888a868935
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-12 15:44:05 +0530

    utils logger added

---
+++ identity-service/src/utils/logger.js
@@ -0,0 +1,24 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+    level: process.env.NODE_ENV === "production" ? "info" : "debug",
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+        winston.format.errors({ stack: true }),
+        winston.format.splat()
+    ),
+    defaultMeta: { service: "identity-service" },
+    transports: [
+        new winston.transports.console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            ),
+        }),
+        new winston.transports.File({ filename: "error.log", level: "error" }),
+        new winston.transports.File({ filename: "combined.log" }),
+    ],
+});
+
+export default logger;
