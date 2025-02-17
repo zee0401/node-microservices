a10ce7c5a2106c30cf0d4aa93b1de055370f934e
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-17 12:34:51 +0530

    rate limit express

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -1,12 +1,49 @@
-import cors from "cors";
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import redis from "ioredis";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
+import { RedisStore } from "rate-limit-redis";
+
+import logger from "../../identity-service/src/utils/logger.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+const redisClient = new redis(process.env.REDIS_URL);
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+const ratelimit = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+    handler: (req, res) => {
+        logger.warn(
+            "sensitive endpoints rate limit exceeded for IP address",
+            req.ip
+        );
+        res.status(429).json({
+            success: false,
+            message: "Too many requests",
+        });
+    },
+    store: new RedisStore({
+        sendCommand: (...args) => redisClient.call(...args),
+    }),
 });
 
-app.use(cors());
+app.use(ratelimit);
 
-app.listen(3000);
+app.use((req, res, next) => {
+    logger.info(`Received ${req.method} request from ${req.ip} for ${req.url}`);
+    logger.info(`request body: ${req.body}`);
+    next();
+});
