569970886109acb39d4c3046a69fbbed132beafe
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-18 03:11:52 +0530

    login controller

--- identity-service/src/controllers/userController.js
+++ identity-service/src/controllers/userController.js
@@ -1,5 +1,5 @@
 import User from "../models/UserModel.js";
-import { registerValidation } from "../utils/validation.js";
+import { registerValidation, validateLogin } from "../utils/validation.js";
 import logger from "../utils/logger.js";
 import generateToken from "../utils/generateToken.js";
 
@@ -51,6 +51,62 @@ export const registerUser = async (req, res) => {
         });
     } catch (err) {
         logger.error("Registeration failed", err);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
+
+export const loginUser = async (req, res) => {
+    logger.info("Login user Endpoint");
+
+    const validation = validateLogin(req.body);
+    if (validation.error) {
+        logger.warn("validation error", validation.error.details[0].message);
+        return res.status(400).json({
+            success: false,
+            message: validation.error.details[0].message,
+        });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            logger.error("Invalid User");
+            return res.status(400).json({
+                success: false,
+                message: "Invalid User credentials",
+            });
+        }
+
+        const isPasswordMatched = await user.comparePassword(password);
+
+        if (!isPasswordMatched) {
+            logger.error("Invalid Password");
+            return res.status(400).json({
+                success: false,
+                message: "Invalid User credentials",
+            });
+        }
+        const { accessToken, refreshToken } = await generateToken(user);
+
+        logger.info("user logged in successfully");
+
+        return res.status(200).json({
+            success: true,
+            message: "User logged in successfully",
+            data: {
+                accessToken,
+                refreshToken,
+                user: user._id,
+            },
+        });
+    } catch (err) {
+        logger.error("Login failed", err);
         return res.status(500).json({
             success: false,
             message: "Internal server error",
