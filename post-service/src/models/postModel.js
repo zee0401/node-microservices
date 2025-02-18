ce382b14c7bad95b05f6b59ad8bf2f7bf170b013
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-18 19:18:19 +0530

    post model added

---
+++ post-service/src/models/postModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        mediaIds: [
+            {
+                type: String,
+            },
+        ],
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    { timestamps: true }
+);
+postSchema.index({ content: "text" });
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
