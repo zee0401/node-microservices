4a6f97d8fed2143592a7878142c0dc183b85957a
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-18 18:01:51 +0530

    user route for token and logout

--- identity-service/src/routes/userRoutes.js
+++ identity-service/src/routes/userRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/userController.js";
+import {
+    loginUser,
+    logoutUser,
+    refreshTokenUser,
+    registerUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/refresh-token", refreshTokenUser);
+router.post("/logout", logoutUser);
 
 export default router;
