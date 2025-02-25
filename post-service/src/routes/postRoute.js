c59d062f31d57f0cf43d9d96c24466cfdbd634cc
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-25 13:02:13 +0530

    get post and delete post by id

--- post-service/src/routes/postRoute.js
+++ post-service/src/routes/postRoute.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { createPost, getAllPosts } from "../controllers/postController.js";
+import {
+    createPost,
+    deletePostById,
+    getAllPosts,
+    getPostById,
+} from "../controllers/postController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -7,5 +12,7 @@ const router = express.Router();
 router.use(authMiddleware);
 router.post("/create-post", createPost);
 router.get("/all-post", getAllPosts);
+router.post("/:id", getPostById);
+router.delete("/:id", deletePostById);
 
 export default router;
