cca8d6a9a9847f0d24d5cfb13ee9853bd50d0912
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-25 11:16:05 +0530

    post route

--- post-service/src/routes/postRoute.js
+++ post-service/src/routes/postRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPost } from "../controllers/postController.js";
+import { createPost, getAllPosts } from "../controllers/postController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.use(authMiddleware);
 router.post("/create-post", createPost);
+router.get("/all-post", getAllPosts);
 
 export default router;
