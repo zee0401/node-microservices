1792089be3ccf7dc0b289188e8fa336150476104
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-25 13:12:54 +0530

    logger created

---
+++ media-service/src/utils/logger.js
@@ -0,0 +1,24 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+    level: process.env.NODE_ENV === "production" ? "info" : "debug",
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+        winston.format.errors({ stack: true }),
+        winston.format.splat()
+    ),
+    defaultMeta: { service: "post-service" },
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize({ all: true }),
+                winston.format.simple()
+            ),
+        }),
+        new winston.transports.File({ filename: "error.log", level: "error" }),
+        new winston.transports.File({ filename: "combined.log" }),
+    ],
+});
+
+export default logger;
