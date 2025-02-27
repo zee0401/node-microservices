3357f5f74070d34fd74c4f098df80238a874e145
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-02-27 11:33:09 +0530

    cloudinary setup

---
+++ media-service/src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import logger from "./logger";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadMediaToCloudinary = async (file) => {
+    return new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_streaM(
+            {
+                resource_type: "auto",
+            },
+
+            (error, result) => {
+                if (error) {
+                    logger.error("Error while uploading image", error);
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            }
+        );
+        uploadStream.end(file.buffer);
+    });
+};
