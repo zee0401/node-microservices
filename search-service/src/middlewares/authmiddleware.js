f2b552557019d31340c37fb833cdbffe474807a8
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-03-05 03:07:49 +0530

    authmiddleware added

---
+++ search-service/src/middlewares/authmiddleware.js
@@ -0,0 +1,18 @@
+import logger from "../urils/logger.js";
+
+const authmiddleware = (req, res, next) => {
+    const userId = req.headers["x-user-id"];
+
+    if (!userId) {
+        logger.error("Access denied without user id");
+        return res.status(401).json({
+            success: false,
+            message: "Authencation required! Please login to continue",
+        });
+    }
+
+    req.user = { userId };
+    next();
+};
+
+export default authmiddleware;
